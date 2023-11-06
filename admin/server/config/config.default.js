2e5a0b65463f232387b6ab5c4cfeb1e1ccb021b2
xianwanglong.lxw <xianwanglong.lxw@digital-engine.com>
2023-11-06 14:57:58 +0800

    忽略文件删除lock文件

--- admin/server/config/config.default.js
+++ admin/server/config/config.default.js
@@ -26,7 +26,7 @@ module.exports = appInfo => {
     host: 'localhost',
     port: '3306',
     username: 'root',
-    password: 'root',
+    password: 'long206279',
     define: {
       underscored: true,
       freezeTableName: true,
